8eb5ce5751dded471c1c683464a1154fad6cc8cd
Prince Nasamu Alhassan <126364975+NasamuAlhassan@users.noreply.github.com>
2026-04-23 14:13:03 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('showInfoBtn').addEventListener('click', function() {
+    var profileSection = document.getElementById('profileSection');
+    
+    profileSection.innerHTML = 
+        '<h2>Name: Prince Nasamu Alhassan</h2>' +
+        '<p><strong>Course:</strong> Mathematical Science</p>' +
+        '<p><strong>Bio</strong> <br>A Mathematical Science student at the University of Ghana with a focus on Computer Science and data science. Passionate about technology, problem-solving, and creating real impact for the community in Bawku.</p>' +
+        '<button id="showPictureBtn">Show Profile Picture</button>';
+    
+    document.getElementById('showPictureBtn').addEventListener('click', function() {
+        var profileSection = document.getElementById('profileSection');
+        profileSection.innerHTML += '<img src="../ProfilePic.jpg" alt="Profile Picture">';
+    });
+});
